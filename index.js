88bda9fc015fff9015f806a80e9406e67ab9ce87
MrMarans <ron_marvin@yahoo.de>
2023-09-05 13:20:54 +0200

    Test Json

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
+let jsonData; // Die globale Variable, in der die JSON-Daten gespeichert werden
+
 // JSON-Daten aus einer Datei im Browser laden
 fetch('data.json')
   .then(response => response.json())
   .then(data => {
+    // Speichere die geladenen JSON-Daten in der Variable jsonData
+    jsonData = data;
+
     // Hier kannst du mit den geladenen JSON-Daten arbeiten
-    console.log(data);
+    console.log(jsonData);
+
+    // Du kannst jsonData jetzt überall in deinem Code verwenden
+    doSomethingWithJsonData();
   })
   .catch(error => {
     console.error('Fehler beim Laden der JSON-Daten:', error);
   });
+
+// Funktion, die auf die JSON-Daten zugreifen kann
+function doSomethingWithJsonData() {
+  if (jsonData) {
+    // Hier kannst du jsonData verwenden
+    console.log('Daten verfügbar:', jsonData);
+  } else {
+    console.log('Daten sind noch nicht geladen.');
+  }
+}
