bd987f9f9d1b50a067d54945b7cb10dee7449115
MrMarans <ron_marvin@yahoo.de>
2023-09-05 13:21:04 +0200

    testjson

--- index.js
+++ index.js
@@ -2,27 +2,35 @@ let jsonData; // Die globale Variable, in der die JSON-Daten gespeichert werden
 
 // JSON-Daten aus einer Datei im Browser laden
 fetch('data.json')
-  .then(response => response.json())
-  .then(data => {
-    // Speichere die geladenen JSON-Daten in der Variable jsonData
-    jsonData = data;
+    .then(response => response.json())
+    .then(data => {
+        // Speichere die geladenen JSON-Daten in der Variable jsonData
+        jsonData = data;
 
-    // Hier kannst du mit den geladenen JSON-Daten arbeiten
-    console.log(jsonData);
+        // Hier kannst du mit den geladenen JSON-Daten arbeiten
+        console.log(jsonData);
 
-    // Du kannst jsonData jetzt überall in deinem Code verwenden
-    doSomethingWithJsonData();
-  })
-  .catch(error => {
-    console.error('Fehler beim Laden der JSON-Daten:', error);
-  });
+        // Du kannst jsonData jetzt überall in deinem Code verwenden
+        doSomethingWithJsonData();
+    })
+    .catch(error => {
+        console.error('Fehler beim Laden der JSON-Daten:', error);
+    });
 
 // Funktion, die auf die JSON-Daten zugreifen kann
 function doSomethingWithJsonData() {
-  if (jsonData) {
-    // Hier kannst du jsonData verwenden
-    console.log('Daten verfügbar:', jsonData);
-  } else {
-    console.log('Daten sind noch nicht geladen.');
-  }
+    if (jsonData) {
+        // Hier kannst du jsonData verwenden
+        console.log('Daten verfügbar:', jsonData);
+
+        // Iteriere durch jedes Projekt und greife auf die "phase" zu
+        Object.keys(deinJSON).forEach(projekt => {
+            const phase = deinJSON[projekt].phase;
+            console.log(`Projekt: ${projekt}`);
+            console.log(`Phase: ${phase.join(', ')}`);
+        });
+
+    } else {
+        console.log('Daten sind noch nicht geladen.');
+    }
 }
